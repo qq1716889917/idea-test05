5b5b4dc0f4fa02785f2b745913bd8b828c3238d8
wangdapao <1716889917@qq.com>
2020-01-08 21:53:41 +0800

    测试修改一下下

--- springboot-jpa-thymeleaf-day02-01/src/main/java/com/wzt/controller/UserController.java
+++ springboot-jpa-thymeleaf-day02-01/src/main/java/com/wzt/controller/UserController.java
@@ -8,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+/*萨达所大所大所大所大所大所大所大所*/
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
